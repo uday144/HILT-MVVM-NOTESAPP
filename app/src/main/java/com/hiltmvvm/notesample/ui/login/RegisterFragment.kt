32f5ce89eb20f0108b8b5687a94ddc226bdec5e2
uday singh <>
2024-03-28 15:43:03 +1100

    Added Registration fragment

--- app/src/main/java/com/hiltmvvm/notesample/ui/login/RegisterFragment.kt
+++ app/src/main/java/com/hiltmvvm/notesample/ui/login/RegisterFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hiltmvvm.notesample.R
+import com.hiltmvvm.notesample.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
 
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        
+        return binding.root
     }
 }
