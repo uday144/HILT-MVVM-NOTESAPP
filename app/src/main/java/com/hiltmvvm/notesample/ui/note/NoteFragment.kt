0bee2301338e382f297b3e0497134a57f16570fa
Uday Singh <uday144@gmail.com>
2024-03-30 01:28:25 +1100

    Add note API impplementation

--- app/src/main/java/com/hiltmvvm/notesample/ui/note/NoteFragment.kt
+++ app/src/main/java/com/hiltmvvm/notesample/ui/note/NoteFragment.kt
@@ -5,15 +5,93 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.google.gson.Gson
 import com.hiltmvvm.notesample.R
+import com.hiltmvvm.notesample.databinding.FragmentNoteBinding
+import com.hiltmvvm.notesample.models.NoteRequest
+import com.hiltmvvm.notesample.models.NoteResponse
+import com.hiltmvvm.notesample.utils.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
+    private val noteViewModel by viewModels<NoteViewModel>()
+    private var note: NoteResponse? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setInitialData()
+        bindHandlers()
+        bindObservers()
+    }
+
+    private fun bindObservers() {
+        noteViewModel.statusLiveData.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is NetworkResult.Success -> {
+                    findNavController().popBackStack()
+                }
+                is NetworkResult.Error -> {
+
+                }
+                is NetworkResult.Loading -> {
+
+                }
+            }
+        })
+    }
+
+    private fun bindHandlers() {
+        binding.btnDelete.setOnClickListener {
+            note?.let { noteViewModel.deleteNote(it!!._id) }
+        }
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val title = txtTitle.text.toString()
+                val description = txtDescription.text.toString()
+                val noteRequest = NoteRequest(title, description)
+                if (note == null) {
+                    noteViewModel.createNote(noteRequest)
+                } else {
+                    noteViewModel.updateNote(note!!._id, noteRequest)
+                }
+            }
+        }
+    }
+
+    private fun setInitialData() {
+        val jsonNote = arguments?.getString("note")
+        if (jsonNote != null) {
+            note = Gson().fromJson<NoteResponse>(jsonNote, NoteResponse::class.java)
+            note?.let {
+                binding.txtTitle.setText(it.title)
+                binding.txtDescription.setText(it.description)
+            }
+        }
+        else{
+            binding.addEditText.text = resources.getString(R.string.add_note)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
 }
