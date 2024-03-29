ffd2de870c2306499c87014b4770f53b4f31e1df
Uday Singh <uday144@gmail.com>
2024-03-30 00:44:07 +1100

    Added okHttp interceptor

--- app/src/main/java/com/hiltmvvm/notesample/di/NetworkModule.kt
+++ app/src/main/java/com/hiltmvvm/notesample/di/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.hiltmvvm.notesample.di
 
+import com.hiltmvvm.notesample.api.AuthInterceptor
+import com.hiltmvvm.notesample.api.NoteAPI
 import com.hiltmvvm.notesample.api.UserAPI
 import com.hiltmvvm.notesample.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,17 +17,30 @@ import javax.inject.Singleton
 @Module
 class NetworkModule {
 
+
     @Singleton
     @Provides
-    fun providesRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
+    fun providesRetrofit(): Retrofit.Builder {
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
     }
+
     @Singleton
     @Provides
-    fun providesUserAPI(retrofit: Retrofit): UserAPI {
-        return retrofit.create(UserAPI::class.java)
+    fun provideOkHttpClient(interceptor: AuthInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
     }
+
+    @Singleton
+    @Provides
+    fun providesUserAPI(retrofitBuilder: Retrofit.Builder): UserAPI {
+        return retrofitBuilder.build().create(UserAPI::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesNoteAPI(retrofitBuilder: Retrofit.Builder, okHttpClient: OkHttpClient): NoteAPI {
+        return retrofitBuilder.client(okHttpClient).build().create(NoteAPI::class.java)
+    }
+
 }
